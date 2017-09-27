934354ddd83252ff526c68e5169b55ef623a39d4
maxmyth01 <maxmyth01@gmail.com>
2017-09-27 13:34:47 -0400

    Updated from Brython Server: 9/27/2017 1:34:46 PM

--- loopDemo.py
+++ loopDemo.py
@@ -11,8 +11,18 @@ for i in range(1,5):
 # print the numbers from 1-20
 for i in range(1,21):
     print(i)
-"""
+
 
 #print out the odd numbers from 13-27
-for i in range(13,28,2):
+for i in range(13,28,2): # go up by 2 each time
         print(i)
+        
+"""
+        
+#add up the numbers from 1-5
+total = 0
+for i in range(1,6):
+    total = total+1
+    
+print(total)
+    

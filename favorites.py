78c1403991f8a275def450bc63e31ab837976f2b
maxmyth01 <maxmyth01@gmail.com>
2017-09-27 13:48:56 -0400

    Updated from Brython Server: 9/27/2017 1:48:55 PM

--- favorites.py
+++ favorites.py
@@ -1,1 +1,10 @@
+#Max Low
+#9-27-17
+#favorites.py -- pick a word and repeat for number of times
+
+word = input('Enter your favorite word: ')
+num = input('Enter your favorite number: ')
+i=0
+while i in range(0,num):
+    print(word)
 

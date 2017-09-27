167e08801256154e9b3cf377b98161553035d37b
maxmyth01 <maxmyth01@gmail.com>
2017-09-27 13:32:40 -0400

    Updated from Brython Server: 9/27/2017 1:32:38 PM

--- loopDemo.py
+++ loopDemo.py
@@ -4,16 +4,15 @@
 
 """
 #print I love computer science 5 times
-for i in range(1-5):
+for i in range(1,5):
     print('I love computer science')
 
 
 # print the numbers from 1-20
-for i in range(1-21):
+for i in range(1,21):
     print(i)
 """
 
 #print out the odd numbers from 13-27
-for i in range(13-28):
-    if i % 2 == 1:
+for i in range(13,28,2):
         print(i)

f3c8b1bf827fb60c71758d0206d0d39100c6f1e8
maxmyth01 <maxmyth01@gmail.com>
2017-09-27 13:54:09 -0400

    Updated from Brython Server: 9/27/2017 1:54:06 PM

--- fives.py
+++ fives.py
@@ -2,7 +2,7 @@
 #9-27-17
 #fives.py -- rint the mulitples of 5 up to user input
 
-num = int(input('Enter a number: ))
+num = int(input('Enter a number: '))
 i=0
 for i in range (0,num):
     print(i*5)

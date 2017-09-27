e23523081593dd7114c33c32bcc40bacf033bc2e
maxmyth01 <maxmyth01@gmail.com>
2017-09-27 13:44:38 -0400

    Updated from Brython Server: 9/27/2017 1:44:34 PM

--- loopDemo.py
+++ loopDemo.py
@@ -26,17 +26,22 @@ while i<=20:
 for i in range(13,28,2): # go up by 2 each time
         print(i)"""
         
-i =13
+"""i =13
 while i <=27:
     print(i)
-    i = i + 2
+    i = i + 2"""
 
         
 """#add up the numbers from 1-5
 total = 0
 for i in range(1,6):
     total = total+i
-    
 print(total)
 """
     
+total = 0
+i =1 
+while i <= 5:
+    total = total +1
+    i = i+1
+print(total)

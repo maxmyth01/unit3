f49dd312aff9427788e0579bca0ff65c3adec8d6
maxmyth01 <maxmyth01@gmail.com>
2017-09-28 14:49:38 -0400

    Updated from Brython Server: 9/28/2017 2:49:37 PM

--- gauss.py
+++ gauss.py
@@ -1,1 +1,14 @@
+#Max Low
+#9-28-17
+#gauss.py -- count the numbers as a series, user control start, stop and intervil
+
+start = int(input('Enter a starting value: '))
+end = int(input('Enter a final value: '))
+step = int(input('Enter the step: '))
+
+i=1
+while i <= end:
+    total = total + i
+    i = i + step
+    print(total)
 

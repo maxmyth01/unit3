8ef2acad08c1856b81bb916d009c56d565803316
maxmyth01 <maxmyth01@gmail.com>
2017-09-28 14:19:55 -0400

    Updated from Brython Server: 9/28/2017 2:19:54 PM

--- warmup6.py
+++ warmup6.py
@@ -1,10 +1,10 @@
 #Max Low
 #9-28-17
 #warmup6.py -- 10 * mississippi
-i=1
+"""i=1
 while i < 11:
     print(i,'mississippi')
     i =+ 1
 """
 for i < 11:
-    print(i, 'mississippi')"""
+    print(i, 'mississippi')

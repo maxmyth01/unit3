cef260f0286e4cf23594316a109fe31ab757415d
maxmyth01 <maxmyth01@gmail.com>
2017-09-29 14:43:52 -0400

    Updated from Brython Server: 9/29/2017 2:43:51 PM

--- numberGuessingGame.py
+++ numberGuessingGame.py
@@ -1,1 +1,21 @@
+#Max Low
+#9-29-17
+#numberGuessingGame.py -- guess a number then display eventual 
 
+from random import randint
+
+guess = int(input('Guess a number between 1-100: '))
+num = randint(1,100)
+total = 0
+
+while num != guess:
+    if guess > num:
+        print('Too high')
+    if guess < num:
+        print('Too low')
+    total += 1
+    guess = int(input('Guess a number between 1-100: '))
+
+print('You win')
+    
+    

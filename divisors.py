439ff0336e3d940a6cff33611467596c43cad4f4
maxmyth01 <maxmyth01@gmail.com>
2017-09-29 14:52:46 -0400

    Updated from Brython Server: 9/29/2017 2:52:45 PM

--- divisors.py
+++ divisors.py
@@ -1,1 +1,10 @@
+#Max Low
+#9-29-17
+#divisors.py -- find all the divisors from user input
 
+num = int(input('Enter a number: '))
+i = 1
+while i != num:
+    if num % i == 0
+        print(i)
+print(num)

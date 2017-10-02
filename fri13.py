0df0169b802035417a9e7d738f33b80a68c38699
maxmyth01 <maxmyth01@gmail.com>
2017-10-02 14:51:44 -0400

    Updated from Brython Server: 10/2/2017 2:51:43 PM

--- fri13.py
+++ fri13.py
@@ -1,11 +1,21 @@
 #Max Low
 #10-2-17
-#fri13 -- find friday the 13 in the next 10 years
+#fri13 -- find friday the 13 for next 10
 
 from datetime import date
 from calendar import weekday
 
 today = date.today().day, date.today().month, date.today().year
 print(today)
-day = weekday(date.today().year,date.today().month,date.today().day)
+day = weekday(date.today().year,date.today().month,13)
 print(day)
+
+"""
+i=0
+while i< 1:
+    day = weekday(date.today().year,date.today().month,13)
+print(day)
+    if day == 4 and date.today().day == 13:
+        print(today)
+        i+=1
+"""

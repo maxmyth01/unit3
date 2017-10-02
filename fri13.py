9207e814ad4a6b8ea475e897a515c84a627b323b
maxmyth01 <maxmyth01@gmail.com>
2017-10-02 14:53:49 -0400

    Updated from Brython Server: 10/2/2017 2:53:48 PM

--- fri13.py
+++ fri13.py
@@ -7,7 +7,7 @@ from calendar import weekday
 
 today = date.today().day, date.today().month, date.today().year
 print(today)
-day = weekday(date.today().year,date.today().month,13)
+day = weekday(date.today().year,date.today().month,date.today().day)
 print(day)
 
 """

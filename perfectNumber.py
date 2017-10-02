692f532c6a15582e91b0ab8086b79f4fdc69f9a0
maxmyth01 <maxmyth01@gmail.com>
2017-10-02 14:27:32 -0400

    Updated from Brython Server: 10/2/2017 2:27:31 PM

--- perfectNumber.py
+++ perfectNumber.py
@@ -1,1 +1,14 @@
+#Max Low
+#10-2-17
+#perfectNumber.py -- determine if user input == perfect num
+
+perfect = int(input('Enter a Number: '))
+
+i=1
+while i<= perfect:
+    if perfect%i == 0:
+        total = total+i
+    i += 1
+print(total)
+
 

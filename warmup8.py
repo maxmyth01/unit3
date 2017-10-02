5d3e7d78c1d2c812606a950d69f56b1b0dac6233
maxmyth01 <maxmyth01@gmail.com>
2017-10-02 14:18:41 -0400

    Updated from Brython Server: 10/2/2017 2:18:40 PM

--- warmup8.py
+++ warmup8.py
@@ -1,1 +1,11 @@
+#Max Low
+#10-2-17
+#warmup8.py -- find the sum of all intigers less than 100,000 that are divisible by 3, 10, and 17
+
+
+i=0
+while i =< 100000:
+    if i //(3*10*17) ==0:
+        print i
+    i+= 1
 

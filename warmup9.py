20bcd3d7ba6ecc3d61476af4764216c4f77ac39f
maxmyth01 <maxmyth01@gmail.com>
2017-10-04 13:46:41 -0400

    Updated from Brython Server: 10/4/2017 1:46:40 PM

--- warmup9.py
+++ warmup9.py
@@ -1,1 +1,22 @@
+#Max Low
+#10-4-17
+#warmup9.py - asks for a word and prints out letters in capitals
 
+word = input('Enter a word: ')
+
+for ch in word:
+    if ch == 'a':
+        print(text.upper())
+    elif ch == 'e':
+        print(text.upper())
+    elif ch == 'i':
+        print(text.upper())
+    elif ch == 'o':
+        print(text.upper())
+    elif ch == 'u':
+        print(text.upper())
+    else:
+        print(ch)
+
+
+    

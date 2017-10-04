ea2fe138b0db80a68797d1b391f7ee3f944c1c33
maxmyth01 <maxmyth01@gmail.com>
2017-10-04 13:57:28 -0400

    Updated from Brython Server: 10/4/2017 1:57:27 PM

--- fri13.py
+++ fri13.py
@@ -9,13 +9,14 @@ today = date.today().day, date.today().month, date.today().year
 print(today)
 day = weekday(date.today().year,date.today().month,date.today().day)
 print(day)
+month = date.today().month
 
 
 i=0
 while i< 5:
-    day = weekday(date.today().year,date.today().month,13)
+    day = weekday(date.today().year,month,13)
     print(day)
     if day == 4:
         print('fri 13')
         i+=1
-    date.today().month = date.today().month + 1 
+    month = month + 1 

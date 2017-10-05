f1b1e4adec0cac446d1b5641b57d260d97d81d9b
maxmyth01 <maxmyth01@gmail.com>
2017-10-05 14:52:51 -0400

    Updated from Brython Server: 10/5/2017 2:52:49 PM

--- changeComputerLoop.py
+++ changeComputerLoop.py
@@ -1,1 +1,28 @@
+#Max Low
+#10-5-17
+#changeComputer.py convert to change with loop
 
+change = int(input('Enter an amount of change: '))
+q= 0
+d= 0
+n= 0
+p= 0
+
+while change > 25:
+    change - 25 
+    q += 1
+    
+while change > 10
+    change - 10
+    d += 1
+    
+while change > 5
+    change - 5
+    n += 1
+    
+while change >= 1
+    change - 1
+    p += 1
+    
+
+    

3b7a95d9247c9e8ab7a8d2d3342c765e97c0d9dc
maxmyth01 <maxmyth01@gmail.com>
2017-10-12 14:26:49 -0400

    Update fri13.py

--- fri13.py
+++ fri13.py
@@ -4,22 +4,22 @@
 
 from datetime import date
 from calendar import weekday
-
+"""
 today = date.today().day, date.today().month, date.today().year
-
 day = weekday(date.today().year,date.today().month,date.today().day)
-
-month = date.today().month
-year = date.today().year
+"""
+month = date.today().month # defines month based on current month and stores as varible so it can be changed
+year = date.today().year # defines month based on current month and stores as varible so it can be changed
 
 
 i=0
-while i< 10:
-    day = weekday(year,month,13)
-    if day == 4:
-        print(month, '13', year)
-        i+=1
-    month = month + 1 
-    if month >12:
-        month = 1
-        year += 1 
+while i< 10: #loop that runs until 10 fri13 are found 
+    day = weekday(year,month,13) """ defines a varible that finds the day of the week as a numerical value
+presets day to only look at the 13th's of each month"""
+    if day == 4: # checks to see if the daty is a friday
+        print(month, '13', year) # prints off day if it meets restrictions of fri 13
+        i+=1 # changes the amount of fri 13 found by one 
+    month = month + 1 # changes to the next month to continue searching 
+    if month >12: # can't have 13 months, resets if exceeding more months than in a year
+        month = 1 #resets month to start of year if nessary
+        year += 1 #increases year by one

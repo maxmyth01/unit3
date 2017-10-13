76398f4d211242e39b6bb6f7b4e307fdf6c51080
maxmyth01 <maxmyth01@gmail.com>
2017-10-13 14:23:59 -0400

    Updated from Brython Server: 10/13/2017 2:23:58 PM

--- quiz3.py
+++ quiz3.py
@@ -1,1 +1,11 @@
+#Max Low
+#10-13-17
+#quiz3.py -- numbers -5:5 for loop, 18:32 by 2 while loop, odd sum of numbers 13:331, asks for word until it contains z
 
+for i in range(-5,5):
+    print(i)
+    
+j = 18
+while j <= 32:
+    print(j)
+    j += 2
